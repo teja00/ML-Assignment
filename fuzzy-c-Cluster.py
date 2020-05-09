51ab3d04ec03aa6737afc4f1815a031250faa429
teja00 <tejanagubandi00@gmail.com>
2020-05-09 17:46:51 +0530

    updated the membership matrix

--- fuzzy-c-Cluster.py
+++ fuzzy-c-Cluster.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import random
 import math
+import operator
 
 Data = pd.read_excel("DataSets.xlsx",skip_blank_lines = False,error_bad_lines=False)
 data = Data.to_numpy()
@@ -18,15 +19,19 @@ e = 0.001
 n = len(data)
 k = 2 
 
-def intial_membership_matrix():
+
+te = initial_membership_matrix()
+ce = compute_centers(te)
+d = find_distances(ce)
+
+def initial_membership_matrix():
     membership_matrix = list()
     for i in range(n):
         rand_list = [random.random() for i in range(k)]
         summation = sum(rand_list)
         temp_list = [x/summation for x in rand_list]
         membership_matrix.append(temp_list)
-    return membership_matrix
-
+    return membership_matrix 
 def compute_centers(membership_matrix):
     v= list()
     for i in range(k):
@@ -38,7 +43,42 @@ def compute_centers(membership_matrix):
         x = [x/denominator for x in numerator]
         v.append(x)
     return v
+def find_distances(centers):
+    Dist = list()
+    for i in range(k):
+        li = list()
+        for j in range(n):
+            arr = np.subtract(data[j],centers[i])
+            arr1 = np.transpose(arr)
+            dist = np.dot(arr1,arr)
+            dis = np.sqrt(dist)
+            li.append(dis)
+        Dist.append(li)
+    return Dist
+def update_membership_matrix(dist,mem):
+    dist = np.transpose(dist)
+    mem = np.array(mem)
+    for i in range(n):
+        for j in range(k):
+            if dist[i][j] > 0:
+                val = dist[i][j]
+                su = 0
+                for l in range(k):
+                    su = su + (math.pow((val/dist[i][l]),(2/m-1)))
+                mem[i][j] = 1/su
+            else:
+                mem[i][j] = 1
+                for s in range(k):
+                    if(s != j):
+                        mem[i][s] = 0              
+    return mem   
 
-        
-            
+    
+    
+
+    
+    
+
+
+
         
